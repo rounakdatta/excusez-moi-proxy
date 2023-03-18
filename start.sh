bcc8c97091025738a3defe1136a117a44c5503df
Rounak Datta <rounakdatta12@gmail.com>
2023-03-18 12:47:49 +0530

    Get docker build running

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
-DB_HOST=localhost DB_USER=postgres DB_PASSWORD=helloworld DB_NAME=xcuzme python3 -m alembic upgrade head
+python3 -m alembic upgrade head
 cd app/
-DB_USER=postgres DB_PASSWORD=helloworld DB_NAME=xcuzme DB_HOST=localhost OPENAI_ORGANIZATION=***REMOVED*** OPENAI_API_KEY=***REMOVED*** python3 -m uvicorn main:app
+python3 -m uvicorn main:app
